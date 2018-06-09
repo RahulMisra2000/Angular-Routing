4994fa43d90bbd0174d606647fc1eada3c1e4262
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-08 22:29:16 -0400

    Comment

--- APM-Final/src/app/products/product.module.ts
+++ APM-Final/src/app/products/product.module.ts
@@ -18,35 +18,14 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      {
-        path: '',
-        component: ProductListComponent
-      },
-      {
-        path: ':id',
-        component: ProductDetailComponent,
-        resolve: { product: ProductResolver }
-      },
-      {
-        path: ':id/edit',
-        component: ProductEditComponent,
-        resolve: { product: ProductResolver },
-        canDeactivate: [ProductEditGuard],
-        children: [
-          {
-            path: '',
-            redirectTo: 'info',
-            pathMatch: 'full'
-          },
-          {
-            path: 'info',
-            component: ProductEditInfoComponent
-          },
-          {
-            path: 'tags',
-            component: ProductEditTagsComponent
-          }
-        ]
+      { path: '',  component: ProductListComponent },
+      { path: ':id', component: ProductDetailComponent,  resolve: { product: ProductResolver } },
+      { path: ':id/edit',  component: ProductEditComponent,   resolve: { product: ProductResolver }, canDeactivate: [ProductEditGuard],
+                        children: [
+                          { path: '', redirectTo: 'info', pathMatch: 'full'   },
+                          { path: 'info', component: ProductEditInfoComponent },
+                          { path: 'tags', component: ProductEditTagsComponent }
+                        ]
       }
     ])
   ],
