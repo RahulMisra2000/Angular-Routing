545aa7544093fb389fd7db02781f62b47fb50e4c
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-09 23:33:01 -0400

    Comment

--- APM-Final/src/app/products/product.service.ts
+++ APM-Final/src/app/products/product.service.ts
@@ -14,6 +14,7 @@ import { IProduct } from './product';
 export class ProductService {
     private baseUrl = 'api/products';
 
+    // Http is deprecated now. Should use HttpClient -----------------------------
     constructor(private http: Http) { }
 
     getProducts(): Observable<IProduct[]> {
