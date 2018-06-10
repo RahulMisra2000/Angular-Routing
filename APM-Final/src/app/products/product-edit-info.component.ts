9b81392bfe9986658461509a5d2b9e02f816b61b
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-09 23:31:23 -0400

    Comment

--- APM-Final/src/app/products/product-edit-info.component.ts
+++ APM-Final/src/app/products/product-edit-info.component.ts
@@ -16,6 +16,8 @@ export class ProductEditInfoComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // Accessing the parent component's resolve data. Getting a reference to it.
+    // So both the parent component and this component are pointing to the SAME data
     this.route.parent.data.subscribe(data => {
       if (this.productForm) {
         this.productForm.reset();
